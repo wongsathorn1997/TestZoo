bcb569a19a4d58e6031a03cd186a2d3b57178a96
wongsathorn <artnote@hotmail.com>
2019-04-18 10:25:23 +0700

    2

--- app/src/main/java/sdu/cs/wongsathorn/testzoo/MainActivity.java
+++ app/src/main/java/sdu/cs/wongsathorn/testzoo/MainActivity.java
@@ -1,6 +1,8 @@
 package sdu.cs.wongsathorn.testzoo;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +12,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 
-    ImageView Image1, Image2, Image3;
+    ImageView Image1, Image2, Image3,Image4,Image5;
     MediaPlayer mediaPlayer1, mediaPlayer2, mediaPlayer3;
 
     @Override
@@ -21,22 +23,38 @@ public class MainActivity extends AppCompatActivity {
         Image1 = findViewById(R.id.imageView2);
         Image2 = findViewById(R.id.imageView3);
         Image3 = findViewById(R.id.imageView4);
-
-
-        mediaPlayer1 = MediaPlayer.create(this, R.raw.elephant);
-        mediaPlayer2 = MediaPlayer.create(this, R.raw.horse);
-        mediaPlayer3 = MediaPlayer.create(this, R.raw.lion);
+        Image4 = findViewById(R.id.imageView5);
+        Image5 = findViewById(R.id.imageView6);
     }
 
     public void playSound(View view) {
         mediaPlayer1.start();
+
+        mediaPlayer1 = MediaPlayer.create(this, R.raw.elephant);
     }
 
     public void playSound2(View view) {
         mediaPlayer2.start();
+
+        mediaPlayer2 = MediaPlayer.create(this, R.raw.horse);
     }
 
     public void playSound3(View view) {
         mediaPlayer3.start();
+
+        mediaPlayer3 = MediaPlayer.create(this, R.raw.lion);
     }
-}
+
+    public void clickMobile(View view) {
+        Intent mobileIntent = new Intent(Intent.ACTION_DIAL);
+        mobileIntent.setData(Uri.parse("tel:0805714197"));
+        startActivity(mobileIntent);
+    }
+    public void clickMap(View view) {
+        Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:13.216298,101.056642(สวนสัตว์เปิดเขาเขียว)");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        startActivity(mapIntent);
+    }
+
+    }
+
